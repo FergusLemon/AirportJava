0e6c717d4ad2d22cf257696f81399dad39052c91
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-10 18:38:54 +0000

    Instruct a plane to land complete

--- src/airport.js
+++ src/airport.js
@@ -1,20 +1,21 @@
 'use strict';
 
 var Airport = function(capacity) {
+  this._hangar = [];
   this.capacity = capacity;
 };
 
 var plane = "plane";
 
-Airport.prototype.planes = function() { return []; };
+Airport.prototype.planes = function() { return this._hangar; };
 
-Airport.prototype.land = function(plane){
-  if (this.planes.length >= this.capacity) {
-    throw new Error('Cannot land: airport is full');
-  }
-  this.planes.push(plane);
+Airport.prototype.clearForLanding = function(plane){
+  // if (this.planes.length >= this.capacity) {
+  //   throw new Error('Cannot land: airport is full');
+  // }
+  this._hangar.push(plane);
 };
 
-Airport.prototype.take_off = function(plane){
-  this.planes.pop(plane);
-};
+// Airport.prototype.take_off = function(plane){
+//   this.planes.pop(plane);
+// };
